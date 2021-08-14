013b925b562652925cbcecf7de577635865880d3
Bui The Anh <bupbui95@gmail.com>
2021-08-14 19:47:18 +0700

    3

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import Container from '@material-ui/core/Container';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './helpers/materialUi';
+import 
 
 const App = () => {
     return (
-       <Provider store={store}>
-           hello world
-       </Provider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <Container maxWidth="md">
+                    <h3 style={{ textAlign: 'center' }}>Todo List Account</h3>
+                    
+                </Container>
+            </ThemeProvider>
+        </Provider>
     );
 };
 
